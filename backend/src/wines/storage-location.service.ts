0351758812ecddd998d8009049b371510118275f
Thomas Mayer <thomas.mayer@imato.de>
2021-05-22 22:30:21 +0200

    WIP

--- backend/src/wines/storage-location.service.ts
+++ backend/src/wines/storage-location.service.ts
@@ -4,10 +4,11 @@ import { VintageInfoService } from './vintage-info.service';
 import { VintageInfoDto } from './dto/vintage-info.dto';
 import { WineDto } from './dto/wine.dto';
 import { BottleHistoryService } from './bottle-history.service';
+import { WineService } from './wine.service';
 
 @Injectable()
 export class StorageLocationService {
-    constructor(private vintageInfoService: VintageInfoService, private bottleHistoryService: BottleHistoryService) { }
+    constructor(private vintageInfoService: VintageInfoService, private bottleHistoryService: BottleHistoryService, private wineService: WineService) { }
 
     async removeStorageLocation(wineId: string, vintage: number, storageLocation: StorageLocationDto): Promise<WineDto> {
         const vintageInfo: VintageInfoDto = await this.vintageInfoService.getVintageInfoByVintage(wineId, vintage);
@@ -24,5 +25,19 @@ export class StorageLocationService {
         this.bottleHistoryService.logBottlesRemoved(vintageInfo, 1);
 
         return await this.vintageInfoService.updateVintageInfo(wineId, vintage, vintageInfo);
+    }
+
+    async getWineByStorageLocation(row: number, shelf: number): Promise<WineDto> {
+        const wines: WineDto[] = await this.wineService.getAllWines();
+
+        for (const wine of wines) {
+            for (const vintageInfo of wine.vintageInfos) {
+                if (vintageInfo.storageLocations.some((storageLocation: StorageLocationDto) => storageLocation.row === +row && storageLocation.shelf === +shelf)) {
+                    return wine;
+                }
+            }
+        }
+
+        return null;
     }
 }
