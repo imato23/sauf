329da72c659b11561b55833553ab8f89845ff862
Thomas Mayer <thomas.mayer@imato.de>
2021-07-21 20:54:03 +0200

    Wein Filter eingebaut

--- frontend/src/app/wines/wine-list/wine-list.component.ts
+++ frontend/src/app/wines/wine-list/wine-list.component.ts
@@ -1,12 +1,14 @@
 import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Wine } from '../shared/models/wine.model';
 import { WineService } from '../shared/services/wine.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 import { distinctUntilChanged, map, shareReplay, startWith, switchMap } from 'rxjs/operators';
 import { NavbarService } from 'src/app/core/shared/navbar.service';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { FormControl } from '@angular/forms';
 import { WineCategory } from '../shared/models/wine-category.model';
+import { MatMenuItem } from '@angular/material/menu';
+import { FilterItem } from '../shared/models/filter-item.model';
 
 @Component({
   selector: 'app-wine-list',
@@ -19,6 +21,15 @@ export class WineListComponent implements OnInit {
   public dummyAvatar = 'assets/no-wine-photo.png';
   public isSearchActive = false;
   public searchControl: FormControl = new FormControl('');
+  public filterItems: FilterItem[] = [
+    { displayName: 'Alle', searchPattern: '', selected: true },
+    { displayName: this.translateWineCategory(WineCategory.RedWine), searchPattern: 'rotwein', selected: false },
+    { displayName: this.translateWineCategory(WineCategory.RoseWine), searchPattern: 'rosewein', selected: false },
+    { displayName: this.translateWineCategory(WineCategory.WhiteWine), searchPattern: 'weißwein', selected: false },
+    { displayName: this.translateWineCategory(WineCategory.SparklingWine), searchPattern: 'schaumwein', selected: false },
+  ];
+
+  public isFilterActive$: Subject<boolean> = new BehaviorSubject<boolean>(false);
 
   public isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -28,9 +39,22 @@ export class WineListComponent implements OnInit {
 
   constructor(private wineService: WineService, private navbarService: NavbarService, private breakpointObserver: BreakpointObserver) {
     this.wines$ = this.processSearchPattern(this.searchControl.valueChanges);
+    this.isFilterActive$.subscribe(x => console.log('Filter active: ' + x));
   }
 
   ngOnInit(): void {
+  }
+
+  public onfilterItemSelected(item: FilterItem): void {
+    for (const filterItem of this.filterItems) {
+      filterItem.selected = false;
+    }
+
+    item.selected = true;
+
+    this.isFilterActive$.next(item.displayName !== 'Alle');
+
+    this.wines$ = this.filterWines(item.searchPattern);
   }
 
   public onToggleNavbar(): void {
@@ -51,12 +75,15 @@ export class WineListComponent implements OnInit {
   }
 
   private filterWines(searchPattern: string): Observable<Wine[]> {
-    return this.wineService.getWines().pipe(
+    const result = this.wineService.getWines().pipe(
       map((wines: Wine[]) => wines.filter((wine: Wine) =>
         searchPattern === '' ||
         wine.name.toLowerCase().includes(searchPattern.toLowerCase()) ||
         this.translateWineCategory(wine.category).toLowerCase().includes(searchPattern.toLowerCase()) ||
-        wine.producer.toLowerCase().includes(searchPattern.toLowerCase()))));
+        wine.producer.toLowerCase().includes(searchPattern.toLowerCase())
+      )));
+
+    return result;
   }
 
   private translateWineCategory(category: WineCategory): string {
