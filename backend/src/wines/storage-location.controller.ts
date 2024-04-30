2f2d73bc1c84c7b89e96cac11149b81f7d88c681
Thomas Mayer <thomas.mayer@imato.de>
2024-04-30 19:52:14 +0200

    Rest API for nexst free storage location added (WIP)

--- backend/src/wines/storage-location.controller.ts
+++ backend/src/wines/storage-location.controller.ts
@@ -1,34 +1,46 @@
-import { Body, Controller, Delete, Param, Post } from '@nestjs/common';
-import { StorageLocationService } from './storage-location.service';
-import { WineDto } from './dto/wine.dto';
-import { StorageLocationDto } from './dto/storage-location.dto';
+import {Body, Controller, Delete, Get, NotFoundException, Param, Post} from '@nestjs/common';
+import {StorageLocationService} from './storage-location.service';
+import {WineDto} from './dto/wine.dto';
+import {StorageLocationDto} from './dto/storage-location.dto';
 
-@Controller('wines/:wineId/vintage-infos/:vintage')
+@Controller()
 export class StorageLocationController {
-    constructor(private storageLocationService: StorageLocationService) { }
+    constructor(private storageLocationService: StorageLocationService) {
+    }
 
-    @Delete('storage-locations/:row/:shelf')
+    @Delete('wines/:wineId/vintage-infos/:vintage/storage-locations/:row/:shelf')
     async removeBottle(
         @Param('wineId') wineId: string,
         @Param('vintage') vintage: number,
         @Param('row') row: number,
         @Param('shelf') shelf: number): Promise<WineDto> {
-        return await this.storageLocationService.removeStorageLocation(wineId, vintage, { row: row, shelf: shelf });
+        return await this.storageLocationService.removeStorageLocation(wineId, vintage, {row: row, shelf: shelf});
     }
 
     /**
      * Determines, if any of the specified storage locations already exist in an other vintage of the specified wine
      * or in an other wine.
-     * @param wineId 
-     * @param vintage 
-     * @param storageLocations 
-     * @returns 
+     * @param wineId
+     * @param vintage
+     * @param storageLocations
+     * @returns
      */
-    @Post('storage-locations/exist')
+    @Post('wines/:wineId/vintage-infos/:vintage/storage-locations/exist')
     async storageLocationsExist(
         @Param('wineId') wineId: string,
         @Param('vintage') vintage: number,
         @Body() storageLocations: StorageLocationDto[]): Promise<boolean> {
         return await this.storageLocationService.storageLocationsExist(wineId, vintage, storageLocations);
+    }
+
+    @Get('wines/storage-locations/next-available')
+    async getNextAvailableStorageLocation(): Promise<StorageLocationDto> {
+        const storageLocation = await this.storageLocationService.getNextAvailableStorageLocation();
+
+        if (!storageLocation) {
+            throw new NotFoundException('All storage locations are occupied', 'All storage locations are occupied');
+        }
+
+        return storageLocation;
     }
 }
--- backend/src/wines/storage-location.service.ts
+++ backend/src/wines/storage-location.service.ts
@@ -8,7 +8,8 @@ import { WineService } from './wine.service';
 
 @Injectable()
 export class StorageLocationService {
-    constructor(private vintageInfoService: VintageInfoService, private bottleHistoryService: BottleHistoryService, private wineService: WineService) { }
+    constructor(private vintageInfoService: VintageInfoService, private bottleHistoryService: BottleHistoryService, private wineService: WineService) {
+    }
 
     async removeStorageLocation(wineId: string, vintage: number, storageLocation: StorageLocationDto): Promise<WineDto> {
         const vintageInfo: VintageInfoDto = await this.vintageInfoService.getVintageInfoByVintage(wineId, vintage);
@@ -27,12 +28,12 @@ export class StorageLocationService {
         return await this.vintageInfoService.updateVintageInfo(wineId, vintage, vintageInfo);
     }
 
-    async storageLocationsExist(excludedWineId: string, exlucedVintage: number, storageLocations: StorageLocationDto[]): Promise<boolean> {
+    async storageLocationsExist(excludedWineId: string, excludedVintage: number, storageLocations: StorageLocationDto[]): Promise<boolean> {
         const wines: WineDto[] = await this.wineService.getAllWines();
 
         for (const wine of wines) {
             for (const vintageInfo of wine.vintageInfos) {
-                if (wine._id.toString() === excludedWineId && vintageInfo.vintage === +exlucedVintage) {
+                if (wine._id.toString() === excludedWineId && vintageInfo.vintage === +excludedVintage) {
                     // Ignore the currently opened wine and vintage
                     continue;
                 }
@@ -44,8 +45,49 @@ export class StorageLocationService {
                     }
                 }
             }
-
-            return false;
         }
+
+        return false;
+    }
+
+    async getNextAvailableStorageLocation(excludedStorageLocations: StorageLocationDto[]): Promise<StorageLocationDto> {
+        const maxRows = 19;
+        const shelvesPerRow = 6;
+
+        const occupiedStorageLocations: StorageLocationDto[] = await this.getOccupiedStorageLocations();
+
+        for (let row = 1; row < maxRows; row++) {
+            const occupiedStorageLocationsInRow: StorageLocationDto[] =
+                occupiedStorageLocations.filter((storageLocation: StorageLocationDto) => storageLocation.row === row);
+
+            if (occupiedStorageLocationsInRow.length >= shelvesPerRow) {
+                // All shelves in row are already occupied, continue with next row
+                continue;
+            }
+
+            for (let shelf = 1; shelf < shelvesPerRow; shelf++) {
+                if (occupiedStorageLocationsInRow.some((storageLocation: StorageLocationDto) => storageLocation.shelf === shelf)) {
+                    continue;
+                }
+
+                if (excludedStorageLocations.some((storageLocation: StorageLocationDto) => storageLocation.row === row && storageLocation.shelf === shelf)) {
+                    continue;
+                }
+
+                return { row: row, shelf: shelf };
+            }
+        }
+
+        return null;
+    }
+
+    private async getOccupiedStorageLocations(): Promise<StorageLocationDto[]> {
+        const wines: WineDto[] = await this.wineService.getAllWines()
+
+        return wines.flatMap((wine: WineDto) =>
+            wine.vintageInfos.flatMap(
+                (vintageInfo: VintageInfoDto) => vintageInfo.storageLocations,
+            ),
+        );
     }
 }
