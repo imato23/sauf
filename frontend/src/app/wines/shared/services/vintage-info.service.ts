c068c07935fae34f4c31d7c4128995844ce99a9a
Thomas Mayer <thomas.mayer@imato.de>
2024-04-30 19:52:47 +0200

    New Button for Prefilling storage location (WIP)

--- frontend/src/app/wines/shared/services/vintage-info.service.ts
+++ frontend/src/app/wines/shared/services/vintage-info.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { VintageInfo } from '../models/vintage-info.model';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { StorageLocation } from '../models/storage-location.model';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {VintageInfo} from '../models/vintage-info.model';
+import {Observable} from 'rxjs';
+import {environment} from 'src/environments/environment';
+import {StorageLocation} from '../models/storage-location.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +11,8 @@ import { StorageLocation } from '../models/storage-location.model';
 export class VintageInfoService {
   private vintageInfoApiUrl = `${environment.apiUrl}/wines`;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
 
   public getAllVintageInfo(wineId: string): Observable<VintageInfo[]> {
     return this.httpClient.get<VintageInfo[]>((this.buildUrl(wineId)));
@@ -36,6 +37,10 @@ export class VintageInfoService {
   public removeBottle(wineId: string, vintage: number, storageLocation: StorageLocation): Observable<any> {
     const url = `${this.buildUrl(wineId)}/${vintage}/storage-locations/${storageLocation.row}/${storageLocation.shelf}`;
     return this.httpClient.delete(url);
+  }
+
+  public getNextAvailableStorageLocation(): Observable<StorageLocation> {
+    return this.httpClient.get<StorageLocation>(`${this.vintageInfoApiUrl}/storage-locations/next-available`);
   }
 
   private buildUrl(wineId: string): string {
