e1d22027cbf986219907220f69c620b45a009628
Thomas Mayer <thomas.mayer@imato.de>
2025-05-04 18:08:14 +0200

    Docker Deployment korrigiert

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+import * as fs from 'node:fs';
+
+const httpsOptions: HttpsOptions = {
+  key: fs.readFileSync('/etc/ssl/sauf.home.imato.de_key.pem'),
+  cert: fs.readFileSync('/etc/ssl/sauf.home.imato.de_cert.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
 
   app.enableCors();
 
@@ -19,5 +26,6 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 // noinspection JSIgnoredPromiseFromCall
 bootstrap();
