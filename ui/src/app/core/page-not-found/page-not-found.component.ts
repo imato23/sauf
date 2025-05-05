a47d3ebc51432ec4a4332173bb5912b996113540
Thomas Mayer <thomas.mayer@imato.de>
2025-05-05 21:18:38 +0200

    Formatted "page not found" component

--- ui/src/app/core/page-not-found/page-not-found.component.ts
+++ ui/src/app/core/page-not-found/page-not-found.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    MatIcon
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
 export class PageNotFoundComponent {
-
 }
