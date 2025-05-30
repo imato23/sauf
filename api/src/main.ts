55dcf52bc299414158eabc558aabd677cd514725
Thomas Mayer <thomas.mayer@imato.de>
2025-05-31 01:15:35 +0200

    Temp

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
 import * as fs from 'node:fs';
-import { version } from '../package.json';
+//import { version } from '../package.json';
 
 const httpsOptions: HttpsOptions = {
   key: fs.readFileSync('/etc/ssl/sauf.home.imato.de_key.pem'),
@@ -18,7 +18,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('S.A.U.F')
     .setDescription('Private wine cellar management')
-    .setVersion(version)
+    .setVersion('')
     .addTag('wines')
     .setContact('Thomas Mayer', '', 'thomas.mayer@imato.de')
     .build();
