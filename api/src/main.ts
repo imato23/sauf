57fb0105572af6a37839175cfefcd15149541b18
Thomas Mayer <thomas.mayer@imato.de>
2025-05-31 00:29:35 +0200

    Swagger API version is set from packages.config json

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
 import * as fs from 'node:fs';
+import { version } from '../package.json';
 
 const httpsOptions: HttpsOptions = {
   key: fs.readFileSync('/etc/ssl/sauf.home.imato.de_key.pem'),
@@ -17,7 +18,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('S.A.U.F')
     .setDescription('Private wine cellar management')
-    .setVersion('1.0')
+    .setVersion(version)
     .addTag('wines')
     .setContact('Thomas Mayer', '', 'thomas.mayer@imato.de')
     .build();
