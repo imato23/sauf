daf8e85ab667478555b5fce02dd01753e91e7a72
Thomas Mayer <thomas.mayer@imato.de>
2025-06-01 17:27:26 +0200

    Temp

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # Write environment variable to /usr/share/nginx/html/config.json
-echo "{\"apiUrl\": \"$API_URL\"}" > /usr/share/nginx/html/de/assets/config.json
+echo "{\"apiUrl\": \"$API_URL1\"}" > /usr/share/nginx/html/de/assets/config.json
+
+envsubst '${API_URL2}' < /etc/nginx/template/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 nginx -g 'daemon off;'
